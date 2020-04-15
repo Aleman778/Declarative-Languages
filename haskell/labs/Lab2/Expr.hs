2b8f0a06989ff24a97bf53502b5c5be1ad7fdf5d
Alexander Mennborg <alexander.mennborg@gmail.com>
2020-04-15 14:10:46 +0200

    Finished and corrected lab 2

---
+++ haskell/labs/Lab2/Expr.hs
@@ -0,0 +1,130 @@
+-- Code to Haskell lab assignment 2 in the course D7012E by Håkan Jonsson
+-- Modified by: Alexander Mennborg
+
+import Data.Char
+
+data EXPR = Const Int
+     | Var String
+     | Op String EXPR EXPR
+     | App String EXPR deriving (Eq, Ord, Show)
+
+parse :: String -> EXPR
+parse = fst . buildexpr
+  where
+    notfirst p (_,[]) = True
+    notfirst p (_,x:xs) = not (p x)
+    
+    buildnumber :: String -> (EXPR,String)
+    buildnumber xs = until (notfirst isDigit) accdigits (Const 0, xs)
+      where
+        accdigits :: (EXPR,String) -> (EXPR,String)
+        accdigits (Const n, y:ys) = (Const(10*n+(ord y - 48)), ys)
+    
+    buildvar :: String -> (EXPR,String)
+    buildvar xs = until (notfirst isLetter) accletters (Var "", xs)
+      where
+        accletters :: (EXPR,String) -> (EXPR,String)
+        accletters (Var s, y:ys) = (Var (s ++[y]), ys)
+    
+    
+    buildexpr :: String -> (EXPR,String)
+    buildexpr xs = until (notfirst (\c -> c=='-' || c=='+')) accterms (buildterm xs)
+      where
+        accterms :: (EXPR,String) -> (EXPR,String)
+        accterms (term, y:ys) = (Op (y:[]) term term1, zs)
+          where
+            (term1,zs) = buildterm ys
+    
+    buildterm :: String -> (EXPR,String)
+    buildterm xs = until (notfirst (\c -> c=='*' || c=='/')) accfactors (buildfactor xs)
+      where
+        accfactors :: (EXPR,String) -> (EXPR,String)  
+        accfactors (fact, y:ys) = (Op (y:[]) fact fact1, zs)
+          where
+            (fact1,zs) = buildfactor ys
+    
+    buildfactor :: String -> (EXPR,String)
+    buildfactor [] = error "missing factor"
+    buildfactor ('(':xs) =  case buildexpr xs of (e, ')':ws) -> (e, ws); _ -> error "missing factor"
+    buildfactor (x:xs)
+      | isDigit x = buildnumber (x:xs)
+      | isLetter x = case buildvar (x:xs) of
+                       (Var s, '(':zs) -> let (e,ws)=buildfactor ('(':zs) in (App s e,ws)
+                       p -> p
+      | otherwise = error "illegal symbol"
+
+unparse :: EXPR -> String
+unparse (Const n) = show n
+unparse (Var s) = s
+unparse (Op oper e1 e2) = "(" ++ unparse e1 ++ oper ++ unparse e2 ++ ")"
+unparse (App name arg) = name ++ "(" ++ (unparse arg) ++ ")"
+
+eval :: EXPR -> [(String,Float)] -> Float
+eval (Const n) _ = fromIntegral n
+eval (Var x) env = case lookup x env of Just y -> y ; _ -> error (x ++ " undefined")
+eval (Op "+" left right) env = eval left env + eval right env
+eval (Op "-" left right) env = eval left env - eval right env
+eval (Op "*" left right) env = eval left env * eval right env
+eval (Op "/" left right) env = eval left env / eval right env
+eval (App "sin" arg) env = sin (eval arg env)
+eval (App "cos" arg) env = cos (eval arg env)
+eval (App "log" arg) env = log (eval arg env)
+eval (App "exp" arg) env = exp (eval arg env)
+
+diff :: EXPR -> EXPR -> EXPR
+diff _ (Const _) = Const 0
+diff (Var id) (Var id2)
+  | id == id2 = Const 1
+  | otherwise = Const 0
+diff v (Op "+" e1 e2) = Op "+" (diff v e1) (diff v e2)
+diff v (Op "-" e1 e2) = Op "-" (diff v e1) (diff v e2)
+diff v (Op "*" e1 e2) =
+  Op "+" (Op "*" (diff v e1) e2) (Op "*" e1 (diff v e2))
+diff v (Op "/" e1 e2) =
+  Op "/" (Op "-" (Op "*" (diff v e1) e1) (Op "*" e1 (diff v e2))) (Op "*" e2 e2)
+diff v (App "sin" arg) = Op "*" (App "cos" arg) (diff v arg)
+diff v (App "cos" arg) = Op "*" (Op "-" (Const 0) (App "sin" arg)) (diff v arg)
+diff v (App "log" arg) = Op "*" (Op "/" (Const 1) arg) (diff v arg)
+diff v (App "exp" arg) = Op "*" (App "exp" arg) (diff v arg)
+diff _ _ = error "can not compute the derivative"
+
+simplify :: EXPR -> EXPR
+simplify (Const n) = Const n
+simplify (Var id) = Var id
+simplify (Op oper left right) =
+  let (lefts,rights) = (simplify left, simplify right) in
+    case (oper, lefts, rights) of
+      ("+",e,Const 0) -> e
+      ("+",Const 0,e) -> e
+      ("*",e,Const 0) -> Const 0
+      ("*",Const 0,e) -> Const 0
+      ("*",e,Const 1) -> e
+      ("*",Const 1,e) -> e
+      ("-",e,Const 0) -> e
+      ("/",e,Const 1) -> e
+      ("-",le,re)     -> if left==right then Const 0 else Op "-" le re
+      (op,le,re)      -> Op op le re
+simplify (App name arg) = (App name (simplify arg))
+
+-- 3. Creating Functions
+mkfun :: (EXPR, EXPR) -> (Float -> Float)
+mkfun (body, (Var arg)) = \x -> eval body [(arg, x)]
+mkfun (_, _) = error "mkfun expects a variable expression as second argument"
+
+-- 4. Newton-Raphson
+findzero :: String -> String -> Float -> Float
+findzero var body x0 = 
+  let bodyExpr = parse body
+      varExpr = (Var var)
+      f  = mkfun (bodyExpr, varExpr)
+      f' = mkfun ((diff varExpr bodyExpr), varExpr)
+  in step f f' x0
+  where
+    step :: (Float -> Float) -> (Float -> Float) -> Float -> Float
+    step f f' x
+      | abs (f x0) <= 0.0001 = x
+      | otherwise = 
+        let dx = (f x)/(f' x)
+        in if ((abs dx) <= 0.000001)
+           then x
+           else (step f f' (x - dx))
